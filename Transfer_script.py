c25b2bf479063cf614c3be4c7e9f978bbed24b87
Hemanth-byte <53725743+Hemanth-byte@users.noreply.github.com>
2020-03-11 14:23:42 +0530

    Add files via upload

--- Transfer_script.py
+++ Transfer_script.py
@@ -145,7 +145,7 @@ def main():
         while not repeat:
             get_gcs_objects(google_access_key_id, google_access_key_secret, temp[i]['bucketName'],
                             temp[i]['objectName'])
-            obj = boto3.resource("s3").Object("gcloudtos3test", temp[i]['objectName'])
+            obj = boto3.resource("s3").Object("", temp[i]['objectName'])
             repeat = etag_compare(obj.e_tag, temp[i]['md5Hash'])
     temp = export2['cloudStorageSink']['files']
     for i in range(len(temp)):
